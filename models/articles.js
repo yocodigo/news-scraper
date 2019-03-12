3ec5f0ebfffbab990e49e1f6e232970bb9c7b854
unknown <jjhernandez94@gmail.com>
2019-03-11 19:44:05 -0700

    UPDATE
    Fixed vulnerabilities

--- models/articles.js
+++ models/articles.js
@@ -1,17 +1,16 @@
 var mongoose = require("mongoose");
 
-// Save a reference to the Schema constructor
+// Save the Schema constructor
 var Schema = mongoose.Schema;
 
 // Using the Schema constructor, create a new UserSchema object
-// This is similar to a Sequelize model
 var ArticleSchema = new Schema({
-  // `title` is required and of type String
+ 
   title: {
     type: String,
     required: true
   },
-  // `link` is required and of type String
+
   link: {
     type: String,
     required: true
@@ -20,17 +19,13 @@ var ArticleSchema = new Schema({
     type: String,
     required: true
   },
-  // `comments` is an object that stores a Comments id
-  // The ref property links the ObjectId to the Comments model
-  // This allows us to populate the Article with an associated Comments
+
   comment: {
     type: Schema.Types.ObjectId,
     ref: "Comments"
   }
 });
 
-// This creates our model from the above schema, using mongoose's model method
 var Article = mongoose.model("Article", ArticleSchema);
 
-// Export the Article model
 module.exports = Article;
